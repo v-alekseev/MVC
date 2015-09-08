9efafbe4a53c958b3fe53cb0086227c083350d87
Vitaly <valekseev@yandex.ru>
2015-09-08 22:59:33 +0300

    new controller HelloWorld

---
+++ WebApplicationMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationMVC.Startup))]
+namespace WebApplicationMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
