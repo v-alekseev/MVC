cded23debc863fac88fb5bacea0fd3e2c912f0d5
Vitaly <valekseev@yandex.ru>
2015-09-09 10:15:58 +0300

    complete Controller part

--- WebApplicationMVC/Controllers/HelloWorldController.cs
+++ WebApplicationMVC/Controllers/HelloWorldController.cs
@@ -13,15 +13,19 @@ namespace WebApplicationMVC.Controllers
 
         public ActionResult Index()
         {
+
             return View();
         } 
  
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
         }
     }
 }
